1230cf9392c67ea6c02c55a3111e60688c32133f
Ricardo <richi@debian>
2025-04-08 13:54:23 -0300

    Workout Buddy App

---
+++ backend/routes/workouts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Workout = require('../models/workoutModel');
+const { 
+  createWorkout, 
+  deleteWorkout, 
+  getWorkout, 
+  getWorkouts, 
+  updateWorkout 
+} = require('../controllers/workoutController');
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+// CREATE a new workout
+router.post('/', createWorkout)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
