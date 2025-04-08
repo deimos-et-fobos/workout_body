d21f1cfb2c82879a9d36257a0af6f4d38777308c
Ricardo <richi@debian>
2025-04-08 15:37:21 -0300

    Login users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,8 +7,17 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  // const user = await User.find().sort({createdAt: -1});
-  res.status(200).json({msg: 'login user'})
+  const { email, password } = req.body
+  try {
+    const user = await User.login(email, password)
+    
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message })
+  }
 }
 
 // signup user
